5ed2b047081eec3c8ff51d2e6f0fd52b7080e436
silverkq7 <vinico365@gmail.com>
2025-02-18 18:42:42 +0000

    fiz um commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
-
-
 //Teste Vinicius
 
 int main() {
-    printf("Desafio carta super trunfo!\n")
+   char estado= 'A';
+   char codigo_da_cidade[30]= "A08";
+   int populacao= ;
+   float Area_em_Km= ;
+   float Pib= ;
+   int Pontos_Turistico= ;
+
+    printf("Desafio carta super trunfo!\n");
+    printf("Carta01 :\n");
+    printf("codigo da cidade :%d,estado");
+    
 
     return 0;
 }
